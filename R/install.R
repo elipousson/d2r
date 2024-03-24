716653e7f864bc2c9a69cf68d97f102a770800c3
your name <eli.pousson@gmail.com>
2024-03-24 12:12:44 -0400

    Add docs for install

--- R/install.R
+++ R/install.R
@@ -1,6 +1,14 @@
 #' Install D2 using the sh script
 #'
-#' This does not work
+#' [d2_install()] downloads and executes a `sh` script provided for installing
+#' D2 in [the Getting Started documentation](https://d2lang.com/tour/install).
+#' This is not the most secure way to install D2. You should consider using
+#' Homebrew, a pre-built Windows installer, or another installation option
+#' listed in [the detailed installation
+#' instructions](https://github.com/terrastruct/d2/blob/master/docs/INSTALL.md).
+#'
+#' @param dry_run If `TRUE` (default), append `"--dry-run"` to the arguments
+#'   passed to the `sh` command.
 #' @export
 d2_install <- function(dry_run = TRUE) {
   exec_wait("curl", args = "--fsSL https://d2lang.com/install.sh")
@@ -8,7 +16,7 @@ d2_install <- function(dry_run = TRUE) {
   args <- "-s --"
 
   if (dry_run) {
-    args <- paste(args, "--dry-run")
+    args <- c(args, "--dry-run")
   }
 
   exec_wait("sh", args = args)
