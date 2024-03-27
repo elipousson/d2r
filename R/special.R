7d5a1794d5bd513be35bf2cc1f279621d48a73a1
your name <eli.pousson@gmail.com>
2024-03-27 14:19:16 -0400

    Rename arguments for d2_sql_table
    Use renamed d2_container instead of d2_map

--- R/special.R
+++ R/special.R
@@ -4,46 +4,77 @@ tempid <- function(..., size = 8, replace = TRUE) {
 
 #' Create a D2 SQL Table Diagram
 #'
-#' @param data A data frame used for column names and types.
-#' @param types A character vector matching the number of columns in data or a
-#'   function used to return the character vector. Defaults to
+#' @param data A data frame used for column names and types. Ignored if
+#'   `col_types` is a named vector. Required if `col_types` is unnamed or a
+#'   function.
+#' @param col_types A named character vector, an unnamed character vector with
+#'   the same length as the number of columns in `data`, or a function that
+#'   returns a character vector based on the columns from `data`. Defaults to
 #'   [vctrs::vec_ptype_abbr]. Other recommended options are
 #'   [vctrs::vec_ptype_full] or [base::typeof]. If `types` is a named character
 #'   vector, the column names of `data` are ignored the names of types used
 #'   instead.
 #' @param visibility One or more of: `r keys_d2[["visibility"]]`. Length is
 #'   recycled to match the number of columns in `data`.
-#' @param .id Table ID passed to [d2_map()]. Defaults to
-#'   `rlang::caller_arg(data)`
-#' @param ... Unused at present.
+#' @param id Diagram container id passed to [d2_container()]. Defaults to
+#'   `NULL`. If `NULL`, `id` is set to `rlang::caller_arg(data)` if `data` is
+#'   supplied or a random string if `data = NULL`.
+#' @param label Diagram label. Defaults to `id`. If supplied, `label` is
+#'   displayed instead of `id`.
+#' @inheritDotParams d2_container -shape
+#' @examples
+#'
+#' d2_sql_table(mtcars[,1:4])
+#'
+#' d2_sql_table(
+#'   label = "col_types Example",
+#'   col_types = c("name" = "char", "num" = "int")
+#' )
+#'
+#' @returns A character string with D2 diagram code using the "sql_type"
+#'   shape.
 #' @export
 d2_sql_table <- function(
     data = NULL,
     visibility = NULL,
-    types = vec_ptype_abbr,
-    .id = caller_arg(data),
+    col_types = vec_ptype_abbr,
+    id = NULL,
+    label = id,
     ...) {
-  if (is_function(types)) {
-    types <- lapply(data, types)
+  if (is_function(col_types)) {
+    col_types <- lapply(data, col_types)
   }
 
-  if (!is_named(types)) {
-    types <- set_names(types, names(data))
+  if (!is_named(col_types)) {
+    col_types <- set_names(col_types, names(data))
   }
+
+  if (!is.null(data)) {
+    id <- id %||% caller_arg(data)
+  } else {
+    id <- id %||% tempid()
+  }
+
+  check_string(id, allow_empty = FALSE)
 
   visibility <- vec_recycle(
     visibility %||% "",
-    size = length(types)
+    size = length(col_types)
   )
 
-  visibility <- arg_match(visibility, keys_d2[["visibility"]], multiple = TRUE)
+  visibility <- arg_match(
+    visibility,
+    values = keys_d2[["visibility"]],
+    multiple = TRUE
+  )
+
   visibility <- sub("#", "\\#", visibility)
 
-  d2_map(
-    paste0(
-      "    ", visibility, names(types), ": ", types
-    ),
-    .id = .id,
-    .shape = "sql_table"
+  d2_container(
+    d2_key_val(val = col_types, key = paste0(visibility, names(col_types))),
+    id = id,
+    label = label,
+    shape = "sql_table",
+    ...
   )
 }
