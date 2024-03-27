b1e5af37de0e1c13e178f66166c680af498f81d3
your name <eli.pousson@gmail.com>
2024-03-27 14:16:01 -0400

    Move obj_check_vector_named helper

--- R/utils.R
+++ R/utils.R
@@ -6,6 +6,53 @@
   )
 }
 
+#' Check if object is a vector with only named elements
+#' @noRd
+obj_check_vector_named <- function(
+    x,
+    ...,
+    nm = NULL,
+    allowed_any = FALSE,
+    allowed_only = !allowed_any,
+    arg = caller_arg(x),
+    call = caller_env()) {
+  vctrs::obj_check_vector(x, arg = arg, call = call)
+
+  supplied_nm <- !is.null(nm)
+  x_has_nm <- has_name(x, nm)
+
+  allowed <- c(
+    all(x_has_nm),
+    # FIXME: add error message for allowed_only
+    allowed_only && all(names(x) %in% nm),
+    allowed_any && any(x_has_nm)
+  )
+
+  if (any(allowed)) {
+    return(invisible(NULL))
+  }
+
+  if (!supplied_nm && is_named(x)) {
+    return(invisible(NULL))
+  }
+
+  msg <- "{.arg {arg}} must be a named vector"
+  what <- "all of"
+
+  if (allowed_any) {
+    what <- "any of"
+  }
+
+  if (supplied_nm) {
+    msg <- "{.arg {arg}} must be a named vector with {what} the names {.str {nm}}"
+  }
+
+  cli_abort(
+    msg,
+    call = call
+  )
+}
+
 #' @noRd
 std_alert_info <- function(x, before = "", after = "", bullets = "i") {
   message <- set_names(
